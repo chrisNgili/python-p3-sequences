43bba1e7f68d948b1bb7c4d2a56bf1104d175d80
chrisngili <ngilichristopher@gmail.com>
2025-05-07 15:43:32 +0300

    tests passed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,51 @@
 #!/usr/bin/env python3
 
+# def print_fibonacci(length):
+#     calculated = []
+#     if length == 0:
+#         print(calculated)       
+#     else:
+#         a,b = 0, 1
+#         while a <= length:
+#             calculated.append(a)
+#             a, b = b, a + b
+#             print(calculated)
+#     pass
+# print(print_fibonacci(10))
+# def print_fibonacci(length):
+#     calculated = []
+#     if length == 0:
+#         print (calculated)
+#     elif length == 1:
+#         print([0])
+#     elif length == 2 :
+#         print([0,1])
+#     else:
+#         a,b = 0,1
+#         for i in range(2,length):
+#             calculated.append(a + b)
+#             print(calculated)
+#             a = b
+#             b = calculated 
+# print(print_fibonacci(10))
+
 def print_fibonacci(length):
-    pass
+    calculated = []
+    if length == 0:
+        print(calculated)
+    elif length == 1:
+        print([0])
+    elif length == 2 :
+        print([0,1])    
+    else:
+        a,b = 0,1
+        calculated.append(a)
+        calculated.append(b)
+        for i in range(2, length):
+            sum = a + b
+            a = b
+            b = sum 
+            calculated.append(sum)    
+        print(calculated)
+    pass        
+print(print_fibonacci(10))
